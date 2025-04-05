72a84f2ae97f1c7e13aac18b8aa6eb3088f93aa0
seth-3 <s.iradukund1@alu.com>
2025-04-05 14:05:59 -0700

    bb

--- python-object_relational_mapping/11-model_state_insert.py
+++ python-object_relational_mapping/11-model_state_insert.py
@@ -22,4 +22,3 @@ if __name__ == "__main__":
     session.commit()
     print(state.id)
     session.close()
-
--- python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -24,4 +24,3 @@ if __name__ == "__main__":
             state[1].__dict__['name']))
 
     session.close()
-
